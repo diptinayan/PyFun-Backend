69f9dc05498cfa2f83739b960b4f6ffc5c9ab88c
Dipti Nayan <diptinayan21@gmail.com>
2018-10-11 00:13:22 +0530

    Create s14_lambda.py

---
+++ stage/one/s14_lambda.py
@@ -0,0 +1,50 @@
+from utils.form import blank_form
+from utils import fields_generate
+
+# YOU SHOULD PROPERLY EDIT FEW SECTION:
+# route:       dict for good Sanic route setup and auto import.
+# data:        dict for present at Frontend
+# answer:      function is to judge stdout result is correct or wrong.
+#              This must be manually write in order to judge.
+
+route = {
+    'type': blank_form, # Right now only provide *blank_form* one kind of form 181004.
+    'url': '/stage/one/lambda',
+    'methods': [ 'GET', 'POST' ]
+}
+data = {
+    'title': ''Lambda,  
+    'description': [
+        'A lambda function is a small anonymous function.',
+        'A lambda function can take any number of arguments, but can only have one expression.
+
+',
+      
+    ],
+   
+    'code': [
+        '_____(\'_____\')'
+    ],
+    'fields': []
+}
+data['fields'] = fields_generate(data)  # NEVER remove this line!!
+
+async def sanic_request(request):
+    try:
+        return override(request)
+    except NameError:
+        global data, route
+        return route['type'](data, request, answer)
+
+
+# def override(request):
+#     pass
+
+def answer(stdout, stderr):
+    try:
+        if stderr != []:
+            return False
+        else:
+            return stdout[0].decode() == 'Lambda\n'
+    except:
+        return False
